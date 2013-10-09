143bce702da6d3fdf7815b5273b9f4cac08ca4fb
jianping <ganjpxm@gmail.com>
2013-10-09 09:03:56 -0700

    1

--- JOne/JOne/WebViewVC.m
+++ JOne/JOne/WebViewVC.m
@@ -29,7 +29,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
-    NSURL *url = [NSURL fileURLWithPath:[[NSBundle mainBundle]pathForResource:@"about.html" ofType:nil]];
+    NSURL *url = [NSURL fileURLWithPath:[[NSBundle mainBundle]pathForResource:@"JqmForm.html" ofType:nil]];
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
     [webView loadRequest:request];
 }
